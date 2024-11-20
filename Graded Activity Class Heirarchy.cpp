ff4ddb6b2aabb631d18f177c5e18c8efdffcf044
Sally Schaffner <schaffners@vpcc.edu>
2024-11-19 23:40:02 -0500

    Added activity name.

--- Graded Activity Class Heirarchy.cpp
+++ Graded Activity Class Heirarchy.cpp
@@ -10,16 +10,18 @@ int main()
 {
     // Create a GradedActivity object. The score is 88.
     GradedActivity test1(88.0);
+    test1.setName("Discusson Post");
 
     // Create a PassFailExam object. There are 100 questions,
     // the student missed 25 of them, and the minimum passing
     // score is 70.
     PassFailExam test2(100, 25, 70.0);
+    test2.setName("Practice Test");
 
     // Display the grade data for both objects.
-    cout << "Test 1:\n";
+    cout << "+++ " << test1.getName() << " +++\n";
     displayGrade(&test1);  // Address of the GradedActivity object
-    cout << "\nTest 2:\n";
+    cout << "+++ " << test2.getName() << " +++\n";
     displayGrade(&test2);  // Address of the PassFailExam object
     return 0;
 }
