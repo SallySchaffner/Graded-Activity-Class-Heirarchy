1a304d9cdc164d9afd15e2089e8044a8b73da795
Sally Schaffner <schaffners@vpcc.edu>
2024-11-19 22:49:10 -0500

    Passing pointers

--- Graded Activity Class Heirarchy.cpp
+++ Graded Activity Class Heirarchy.cpp
@@ -1,20 +1,40 @@
-// Graded Activity Class Heirarchy.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <iomanip>
+#include "PassFailExam.h"
+using namespace std;
+
+// Function prototype
+void displayGrade(const GradedActivity*);
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    // Create a GradedActivity object. The score is 88.
+    GradedActivity test1(88.0);
+
+    // Create a PassFailExam object. There are 100 questions,
+    // the student missed 25 of them, and the minimum passing
+    // score is 70.
+    PassFailExam test2(100, 25, 70.0);
+
+    // Display the grade data for both objects.
+    cout << "Test 1:\n";
+    displayGrade(&test1);  // Address of the GradedActivity object
+    cout << "\nTest 2:\n";
+    displayGrade(&test2);  // Address of the PassFailExam object
+    return 0;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+//***************************************************************
+// The displayGrade function displays a GradedActivity object's *
+// numeric score and letter grade. This version of the function *
+// uses a GradedActivity pointer as its parameter.              *
+//***************************************************************
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+void displayGrade(const GradedActivity* activity)
+{
+    cout << setprecision(1) << fixed;
+    cout << "The activity's numeric score is "
+        << activity->getScore() << endl;
+    cout << "The activity's letter grade is "
+        << activity->getLetterGrade() << endl;
+}
